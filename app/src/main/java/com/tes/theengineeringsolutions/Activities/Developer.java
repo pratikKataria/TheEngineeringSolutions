836cba652ebb21571b08e9edcd348986b753914d
Castell <pratikkatariya3.pk@gmail.com>
2019-12-22 12:31:38 -0500

    Developer layout completed

---
+++ app/src/main/java/com/tes/theengineeringsolutions/Activities/Developer.java
@@ -0,0 +1,57 @@
+package com.tes.theengineeringsolutions.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.tes.theengineeringsolutions.R;
+
+public class Developer extends AppCompatActivity {
+
+    private ImageButton expandBtn;
+    private ImageButton backbtn;
+
+    private TextView textViewBio;
+    private LinearLayout linearLayoutLinks;
+    private boolean expanded = false;
+
+    private void init_fields() {
+        expandBtn = findViewById(R.id.expandCardBtn);
+        backbtn = findViewById(R.id.backBtn);
+        textViewBio = findViewById(R.id.textView_Bio);
+        linearLayoutLinks = findViewById(R.id.linearLayoutLinks);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developer);
+
+        init_fields();
+
+        expandBtn.setOnClickListener(v -> {
+           if (!expanded) show();
+           else collapse();
+        });
+
+        backbtn.setOnClickListener(v-> onBackPressed());
+    }
+
+    private void show() {
+        textViewBio.setVisibility(View.VISIBLE);
+        linearLayoutLinks.setVisibility(View.VISIBLE);
+        expandBtn.setImageDrawable(getDrawable(R.drawable.ic_collapse_white_24dp));
+        expanded = true;
+    }
+
+    private void collapse() {
+        textViewBio.setVisibility(View.GONE);
+        linearLayoutLinks.setVisibility(View.GONE);
+        expandBtn.setImageDrawable(getDrawable(R.drawable.ic_expand_more_black_24dp));
+        expanded = false;
+    }
+}
--- app/src/main/java/com/tes/theengineeringsolutions/Activities/MainActivity.java
+++ app/src/main/java/com/tes/theengineeringsolutions/Activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -11,6 +12,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -128,6 +130,19 @@ public class MainActivity extends AppCompatActivity implements ConnectivityRecei
                     return true;
             }
             return false;
+        });
+
+        mNavigationView.setNavigationItemSelectedListener(menuItem -> {
+         switch (menuItem.getItemId()) {
+             case R.id.home:
+                 startActivity(new Intent(this, MainActivity.class));
+                 break;
+             case R.id.about:
+                 startActivity(new Intent(this, Developer.class));
+                 break;
+         }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+         return true;
         });
     }
 
