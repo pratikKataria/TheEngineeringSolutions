e2c193134ea035c7dfabc8ddecaae824e62639c5
Castell <pratikkatariya3.pk@gmail.com>
2020-03-07 06:05:49 -0500

    $Admin - InboxRecyclerViewAdapter - code clean up

--- app/src/main/java/com/tes/theengineeringsolutions/Adapters/InboxRecyclerViewAdapter.java
+++ app/src/main/java/com/tes/theengineeringsolutions/Adapters/InboxRecyclerViewAdapter.java
@@ -1,7 +1,6 @@
 package com.tes.theengineeringsolutions.Adapters;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,16 +16,8 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.tes.theengineeringsolutions.Models.InboxModel;
 import com.tes.theengineeringsolutions.R;
 
-import java.util.ArrayList;
-
 public class InboxRecyclerViewAdapter extends FirestoreRecyclerAdapter<InboxModel, InboxRecyclerViewAdapter.InboxViewHolder> {
 
-    private static final int EMPTY_VIEW = 0;
-    private static final int INBOX_VIEW = 1;
-
-
-    ArrayList<InboxModel> inboxModelList;
-    LayoutInflater inflater;
     Context context;
 
     // Interface Object
@@ -38,30 +29,11 @@ public class InboxRecyclerViewAdapter extends FirestoreRecyclerAdapter<InboxMode
         this.inboxAdapterListener = inboxAdapterListener;
     }
 
-//    public InboxRecyclerViewAdapter(Context context, ArrayList<InboxModel> inboxModelList, InfoAdapterInterface adapterInterface) {
-//        this.context = context;
-//        this.inboxModelList = inboxModelList;
-//
-//        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//        // Initialize your interface to send updates to fragment.
-//        this.adapterInterface = adapterInterface;
-//    }
-
     @NonNull
     @Override
     public InboxViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view_inbox, parent, false);
         return new InboxViewHolder(view);
-//        RecyclerView.ViewHolder holder;
-//        if (viewType == INBOX_VIEW) {
-//            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view_inbox, parent, false);
-//            holder = new InboxViewHolder(view);
-//        } else {
-//            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.empty_layout, parent, false);
-//            holder = new EmptyView(view);
-//        }
-//        return holder;
     }
 
     @Override
@@ -74,102 +46,24 @@ public class InboxRecyclerViewAdapter extends FirestoreRecyclerAdapter<InboxMode
         }
     }
 
-
-//    @Override
-//    public int getItemViewType(int position) {
-////        if (inboxModelList.size() == 0) {
-////            return EMPTY_VIEW;
-////        } else {
-////            return INBOX_VIEW;
-////        } \
-//        if (getItemCount() > 0) {
-//            return INBOX_VIEW;
-//        } else {
-//            return EMPTY_VIEW;
-//        }
-//    }
-
     @Override
     protected void onBindViewHolder(@NonNull InboxViewHolder holder, int position, @NonNull InboxModel model) {
         holder.setCard(
-                    model.getHeading(),
-                    model.getDescription(),
-                    model.getCreated().toString()
+                model.getHeading(),
+                model.getDescription(),
+                model.getCreated().toString()
         );
         holder.imageButtonDeleteBtn.setOnClickListener(
-                v-> {
-                    holder.deleteDocument(
-                            model.getPostId()
-                    );
+                v -> {
+                    holder.deleteDocument();
 
                     Toast.makeText(context, "item : " + getItemCount(), Toast.LENGTH_SHORT).show();
                 }
         );
-//        switch (holder.getItemViewType()) {
-//            case INBOX_VIEW:
-////                InboxViewHolder viewHolder = (InboxViewHolder) holder;
-//
-////                Log.e("Inbox REcycler view ", inboxModelList.toString());
-//
-////                viewHolder.setCard(
-////                        inboxModelList.get(position).getHeading(),
-////                        inboxModelList.get(position).getDescription(),
-////                        inboxModelList.get(position).getCreated().toString()
-////                );
-////
-////                viewHolder.imageButtonDeleteBtn.setOnClickListener(n -> {
-////                    if (inboxModelList.get(position).getPostId() != null)
-////                        adapterInterface.OnItemClicked(position);
-////                });
-//
-//                break;
-//            case EMPTY_VIEW:
-//                break;
-//        }
     }
 
     public interface InboxAdapterListener {
         void onEmptyStateListener(boolean isEmpty);
-    }
-
-//    @Override
-//    protected void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position, @NonNull InboxModel model) {
-//        switch (holder.getItemViewType()) {
-//            case INBOX_VIEW:
-//                InboxViewHolder viewHolder = (InboxViewHolder) holder;
-//
-//                Log.e("Inbox REcycler view ", inboxModelList.toString());
-//
-//                viewHolder.setCard(
-//                        inboxModelList.get(position).getHeading(),
-//                        inboxModelList.get(position).getDescription(),
-//                        inboxModelList.get(position).getCreated().toString()
-//                );
-//
-//                viewHolder.imageButtonDeleteBtn.setOnClickListener(n -> {
-//                    if (inboxModelList.get(position).getPostId() != null)
-//                        adapterInterface.OnItemClicked(position);
-//                });
-//
-//                break;
-//            case EMPTY_VIEW:
-//                break;
-//        }
-//
-//        if (holder instanceof InboxViewHolder) {
-//            ((InboxViewHolder) holder).setCard(
-//                    model.getHeading(),
-//                    model.getDescription(),
-//                    model.getCreated().toString()
-//            );
-//        }
-//    }
-
-    public class EmptyView extends RecyclerView.ViewHolder {
-
-        public EmptyView(@NonNull View itemView) {
-            super(itemView);
-        }
     }
 
     public class InboxViewHolder extends RecyclerView.ViewHolder {
@@ -195,14 +89,7 @@ public class InboxRecyclerViewAdapter extends FirestoreRecyclerAdapter<InboxMode
             textViewDate.setText(date);
         }
 
-        void deleteDocument(String docID) {
-//            DocumentReference seletedDoc = FirebaseFirestore.getInstance().collection("InboxPost").document(docID);
-//            if (seletedDoc != null) {
-//                seletedDoc.delete().addOnSuccessListener(aVoid -> {
-//                    Toast.makeText(context, "post deleted", Toast.LENGTH_SHORT).show();
-//                }).addOnFailureListener(e -> Toast.makeText(context, "failed to delete post", Toast.LENGTH_SHORT).show());
-//            }
-
+        void deleteDocument() {
             getSnapshots().getSnapshot(getAdapterPosition()).getReference().delete();
         }
     }
