61db6fb051cbebf2462bd57b7dcd3ac0631b87f6
Castell <pratikkatariya3.pk@gmail.com>
2020-03-08 00:00:22 -0500

    $Admin - ViewStudentResultActivity - shows all the test given by student

---
+++ app/src/main/java/com/tes/theengineeringsolutions/Activities/ViewStudentResultActivity.java
@@ -0,0 +1,19 @@
+package com.tes.theengineeringsolutions.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tes.theengineeringsolutions.R;
+
+public class ViewStudentResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_student_result);
+
+
+
+    }
+}
