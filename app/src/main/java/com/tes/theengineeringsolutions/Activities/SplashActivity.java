668b5aaaae64d17c7db61e7464a3ce701fb35605
Castell <pratikkatariya3.pk@gmail.com>
2020-03-23 18:34:51 -0500

    {$Student} - SplashActivity - splash screen ui changed

--- app/src/main/java/com/tes/theengineeringsolutions/Activities/SplashActivity.java
+++ app/src/main/java/com/tes/theengineeringsolutions/Activities/SplashActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.os.Handler;
 import android.widget.ProgressBar;
 
 import com.tes.theengineeringsolutions.R;
@@ -21,11 +22,10 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen_layout);
-//        new Handler().postDelayed(() -> {
+        new Handler().postDelayed(() -> {
             startActivity(new Intent(this, MainActivity.class));
-//        startActivity(new Intent(this, QuizActivity.class));
             finish();
-//        }, 4500);
+        }, 2500);
     }
 
 //    private void isDocPresent() {
